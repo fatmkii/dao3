3c9ccfd85874d2b9808ed65456fec974d3fbad41
fatmkii <47155837@qq.com>
2024-02-06 22:49:17 +0800

    users接口的数据类型处理

--- resources/api/methods/user.ts
+++ resources/api/methods/user.ts
@@ -1,23 +1,30 @@
 import { commonAlova } from '@/api/index';
 
 interface userData {
-    "binggan": {
-        'admin'?: number | null,
-        'admin_forums'?: string | null,
-        "nickname": string | null,
-        "coin": number,
-        "coin_in_bank": number,
-        "use_pingbici": boolean,
-        "new_msg": boolean,
-        "user_lv": number,
-        "locked_ttl": number,
+    binggan: {
+        admin?: number | null,
+        admin_forums?: string | null,
+        nickname: string | null,
+        coin: number,
+        coin_in_bank: number,
+        use_pingbici: boolean,
+        new_msg: boolean,
+        user_lv: number,
+        locked_ttl: number,
     },
-    "my_battle_chara"?: object | null
-    "pingbici": object | null,
-    "my_emoji": object | null
+    my_battle_chara?: {
+        name: string,
+        not_use: boolean
+    }[] | null
+    pingbici: {
+        content_pingbici: string
+        fjf_pingbici: string
+        title_pingbici: string
+    } | null,
+    my_emoji: string | null
 }
 
-const userDataGetter = (binggan: string) => commonAlova.Post<userData>(
+const userDataGetter = (binggan: string) => commonAlova.Post(
     '/api/user/show',
     {
         binggan: binggan
@@ -28,6 +35,17 @@ const userDataGetter = (binggan: string) => commonAlova.Post<userData>(
         params: {},
         localCache: { mode: 'placeholder', expire: 60 * 60 * 1000 },
         hitSource: [],
+        transformData(data: userData, headers) {
+            return {
+                ...data,
+                pingbici: data.pingbici === null ? null : {
+                    content_pingbici: data.pingbici.content_pingbici === null ? null : JSON.parse(data.pingbici.content_pingbici) as string[],
+                    fjf_pingbici: data.pingbici.fjf_pingbici === null ? null : JSON.parse(data.pingbici.fjf_pingbici) as string[],
+                    title_pingbici: data.pingbici.title_pingbici === null ? null : JSON.parse(data.pingbici.title_pingbici) as string[],
+                },
+                my_emoji: data.my_emoji === null ? null : JSON.parse(data.my_emoji) as string[],
+            }
+        }
     }
 )
 
