038602937dd84fdd64a6084e204934f4d6dd2fd0
fatmkii <47155837@qq.com>
2024-02-12 11:00:05 +0800

    useLocalStorageToRef类型排除undefine

--- resources/js/func/localStorageToRef.ts
+++ resources/js/func/localStorageToRef.ts
@@ -1,19 +1,15 @@
-import { ref, watch } from 'vue'
+import { ref, watch, type Ref } from 'vue'
 
 //onMount时读取相应的LocalStorage，赋值给Ref变量
 //Ref变量变动时，写入LocalStorage
-function useLocalStorageToRef<T = string>(key: string, defaultValue?: T) {
+function useLocalStorageToRef<T = string>(key: string, defaultValue: T) {
 
-    const value = ref<T>()
+    const value = ref<T>(defaultValue)
     let valueRaw: string | null = null
 
     valueRaw = localStorage.getItem(key)
     if (valueRaw) {
         value.value = JSON.parse(valueRaw)
-    } else if (defaultValue !== undefined) {
-        value.value = defaultValue
-    } else {
-        value.value = undefined
     }
 
     watch(value, (newValue) => {
