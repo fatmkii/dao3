71a7b0317be6bb8919e874815d157c6d65524706
fatmkii <47155837@qq.com>
2024-03-23 17:43:40 +0800

    重新安装laravel11-删除所有旧文件

--- resources/data/subtitles.ts
+++
@@ -1,2 +0,0 @@
-export const subtitles = ["[公告]", "[闲聊]", "[专楼]", "[刷刷]", "[私密]"]
-export type subtitlesType = "[公告]" | "[闲聊]" | "[专楼]" | "[刷刷]" | "[私密]"
--- resources/vue/errors/404.vue
+++
@@ -1,7 +0,0 @@
-
-<template>
-    <div>嗷……页面被吃掉了！<br>
-        404 Not Found.</div>
-</template>
- 
-
