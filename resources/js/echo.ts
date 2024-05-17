0a9b08ad379785d98f154b5897305ce5c1585414
fatmkii <47155837@qq.com>
2024-05-17 23:14:54 +0800

    feat: websocket链接根据主域名变化

--- resources/js/echo.ts
+++ resources/js/echo.ts
@@ -12,10 +12,11 @@ class echoInstance {
 
     connect() {
         if (this._echoConnector === null) {
+            const wsHost = import.meta.env.VITE_REVERB_HOST_SUBDOMAIN + window.location.hostname;
             this._echoConnector = new Echo({
                 broadcaster: 'reverb',
                 key: import.meta.env.VITE_REVERB_APP_KEY,
-                wsHost: import.meta.env.VITE_REVERB_HOST,
+                wsHost: wsHost,
                 wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
                 wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
                 forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
