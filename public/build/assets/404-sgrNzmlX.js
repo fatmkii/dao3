83b0889fcb4f940b5d1cb1af6b2d21e4e31befd7
fatmkii <47155837@qq.com>
2024-06-13 22:21:06 +0800

    chore: 构建

---
+++ public/build/assets/404-sgrNzmlX.js
@@ -0,0 +1,1 @@
+import{A as n,F as o,z as r,J as s}from"./vendor-B725aRmw.js";import{_ as t}from"./app-B1E9hQ9C.js";const _={},c=s("br",null,null,-1);function e(a,f){return n(),o("div",null,[r("嗷……页面被吃掉了！"),c,r(" 404 Not Found.")])}const d=t(_,[["render",e]]);export{d as default};
