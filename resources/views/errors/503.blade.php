476ce4c28d5146104eb1ad9f94c808fcf58c9ac6
fatmkii <47155837@qq.com>
2024-08-03 07:47:35 +0800

    chore: 维护页面更新

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="zh_CN">
+
+<head>
+<meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="referrer" content="never">
+    <link rel="shortcut icon" href="/favicon.ico">
+    <title>小火锅</title>
+</head>
+
+<body>
+    <div class="container">
+        <div style="height: 200px; width:400px;">
+            小火锅维护中……
+            <br>
+            大约需要1小时，很快回来！
+        </div>
+    </div>
+</body>
+
+<style>
+    .container {
+        height: 80vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    body{
+        background-color: #f3fdf3FF;
+    }
+</style>
+
+</html>
