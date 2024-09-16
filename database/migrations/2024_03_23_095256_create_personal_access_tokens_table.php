fbfc14d272f8c979693b5df520c57ea9935ab8b3
fatmkii <47155837@qq.com>
2024-09-16 15:51:33 +0800

    feat: 调整迁移策略personal_access_tokens

--- database/migrations/2024_03_23_095256_create_personal_access_tokens_table.php
+++ database/migrations/2024_03_23_095256_create_personal_access_tokens_table.php
@@ -11,16 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('personal_access_tokens')) {
+            Schema::create('personal_access_tokens', function (Blueprint $table) {
+                $table->id();
+                $table->morphs('tokenable');
+                $table->string('name');
+                $table->string('token', 64)->unique();
+                $table->text('abilities')->nullable();
+                $table->timestamp('last_used_at')->nullable();
+                $table->timestamp('expires_at')->nullable();
+                $table->timestamps();
+            });
+        } else {
+            //如果已经有这个表格，则什么都不做（避免迁移失败）
+        }
     }
 
     /**
