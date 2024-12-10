4a7a4078fd53ac879c8d1d4526b1718b47bff425
Fat Chan <47155837@qq.com>
2024-12-10 21:48:01 +0800

    fix: 修复

--- resources/vue/AdminCenter/index.ts
+++ resources/vue/AdminCenter/index.ts
@@ -1,7 +1,5 @@
 export { default as TabBanners } from './TabBanners.vue';
 export { default as TabGlobal } from './TabGlobal.vue';
-export { default as TabMedal } from './TabMedal.vue';
-export { default as TabCoin } from './TabCoin.vue';
 export { default as TabActives } from './TabActives.vue';
 export { default as TabActivesSuper } from './TabActivesSuper.vue';
 export { default as TabSuperAdmin } from './TabSuperAdmin.vue';
