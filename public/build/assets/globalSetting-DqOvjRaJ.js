ef41ecc3ba56309ffe9297d39eeb77a3b00fc57d
fatmkii <47155837@qq.com>
2025-01-05 20:00:42 +0800

    chore: 构建

--- public/build/assets/globalSetting-DqOvjRaJ.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./app-D9ZT91Mq.js";const n=e.Get("/api/home_banners/",{name:"homeBannersGetter",params:{},localCache:null}),o=e.Get("/api/new_loudspeaker_enable",{name:"newLoudspeakerEnableGetter",params:{},localCache:null});export{n as h,o as n};
