abf21dffea4829067e7cd97ca716e3a4ff74c710
fatmkii <47155837@qq.com>
2026-05-16 16:12:02 +0800

    chore: 构建

--- public/build/assets/globalSetting-0Y7GFXCX.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./app-CLuq6EFt.js";const n=e.Get("/api/home_banners/",{name:"homeBannersGetter",params:{},localCache:null}),o=e.Get("/api/new_loudspeaker_enable",{name:"newLoudspeakerEnableGetter",params:{},localCache:null});export{n as h,o as n};
