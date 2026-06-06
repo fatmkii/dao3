3071dc89b0922a4d58eeb33511f97c488db3f723
fatmkii <47155837@qq.com>
2026-06-06 14:01:44 +0800

    chore: 构建

---
+++ public/build/assets/globalSetting-DuxNeBm8.js
@@ -0,0 +1,1 @@
+import{c as e}from"./app-CjPVsRzb.js";const n=e.Get("/api/home_banners/",{name:"homeBannersGetter",params:{},localCache:null}),o=e.Get("/api/new_loudspeaker_enable",{name:"newLoudspeakerEnableGetter",params:{},localCache:null});export{n as h,o as n};
