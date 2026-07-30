eeae9d5fcebb550c083f16ff5ef3c27d7fdd9b70
fatmkii <47155837@qq.com>
2026-07-30 22:01:11 +0800

    chore: 503页面通过.env显示提示

--- config/app.php
+++ config/app.php
@@ -134,6 +134,7 @@ return [
     'maintenance' => [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
+        'message' => env('APP_MAINTENANCE_MESSAGE', ''),
     ],
 
 ];
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,7 +2,7 @@
 <html lang="zh_CN">
 
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="referrer" content="never">
@@ -15,7 +15,7 @@
         <div style="height: 200px; width:400px;">
             小火锅维护中……
             <br>
-            大概需要1小时，到9点左右。很快回来！
+            {{ config('app.maintenance.message') }}
         </div>
     </div>
 </body>
@@ -27,7 +27,8 @@
         align-items: center;
         justify-content: center;
     }
-    body{
+
+    body {
         background-color: #f3fdf3FF;
     }
 </style>
